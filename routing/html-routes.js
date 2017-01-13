58df51aa0eb20b2460b38a48e9b0bc13d9193601
Tasha Casagni <casagni708@hotmail.com>
2017-01-12 18:38:02 -0600

    base build

---
+++ routing/html-routes.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = function (app) {
+  // Main "/" Route. This will redirect the user to our rendered React application
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+
+    // Main "/" Route. This will redirect the user to our rendered React application
+  app.get("/notes", function(req, res) {
+    res.sendFile(__dirname + "/public/saved.html");
+  });
+
+
+    // Main "/" Route. This will redirect the user to our rendered React application
+  app.get("/builder", function(req, res) {
+    res.sendFile(__dirname + "/public/builder.html");
+  });
+
+};
+
+
